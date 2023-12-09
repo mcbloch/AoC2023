c63a44b25612410f703af8e0db6fd77cec0db864
mcbloch <git@mcbloch.dev>
2023-12-09 16:43:14 +0100

    more functional

--- day09/day09.v
+++ day09/day09.v
@@ -1,70 +1,61 @@
 module main
 
 import os
-import time as tme { new_stopwatch }
-// import math
-import arrays { reduce, window }
-// import maps
+import time { new_stopwatch }
+import arrays { reduce, sum, window }
 
 fn part01(lines []string) !i64 {
-	mut sum := 0
-	for line in lines {
-		mut diffs := line.split(' ').map(it.int())
-		mut last := [diffs.last()]
-		for (diffs.any(it != 0)) {
-			diffs = window(diffs, size: 2).map(it[1] - it[0])
-			last << diffs.last()
-		}
-		sum += reduce(last.reverse(), fn (acc int, elem int) int {
-			return elem + acc
-		})!
-	}
-
-	return sum
+	return sum(lines
+		.map(it.split(' ').map(it.int()))
+		.map(fn (line_int []int) int {
+			mut diffs := line_int.clone()
+			mut last := [diffs.last()]
+			for (diffs.any(it != 0)) {
+				diffs = window(diffs, size: 2).map(it[1] - it[0])
+				last << diffs.last()
+			}
+			return reduce(last.reverse(), fn (acc int, elem int) int {
+				return elem + acc
+			}) or { 0 }
+		}))!
 }
 
-fn part02(lines []string) !i64 {
-	mut sum := 0
-	for line in lines {
-		mut diffs := line.split(' ').map(it.int())
-		mut first := [diffs.first()]
-		for (diffs.any(it != 0)) {
-			diffs = window(diffs, size: 2).map(it[1] - it[0])
-			first << diffs.first()
-		}
-		sum += reduce(first.reverse(), fn (acc int, elem int) int {
-			return elem - acc
-		})!
-	}
-
-	return sum
+fn part02(lines []string) !int {
+	return sum(lines
+		.map(it.split(' ').map(it.int()))
+		.map(fn (line_int []int) int {
+			mut diffs := line_int.clone()
+			mut first := [diffs.first()]
+			for (diffs.any(it != 0)) {
+				diffs = window(diffs, size: 2).map(it[1] - it[0])
+				first << diffs.first()
+			}
+			return reduce(first.reverse(), fn (acc int, elem int) int {
+				return elem - acc
+			}) or { 0 }
+		}))!
 }
 
 fn main() {
-	inputfile := if os.args.len < 2 {
-		'input/09.txt'
-	} else {
-		os.args[1]
+	inputfile := match true {
+		os.args.len >= 2 { os.args[1] }
+		else { 'input/09.txt' }
 	}
 	println('Reading input: ${inputfile}')
 	lines := os.read_lines(inputfile)!
 
 	if os.args.len >= 3 {
-		part := os.args[2]
-		if part == '1' {
-			print(part01(lines)!)
-		} else if part == '2' {
-			print(part02(lines)!)
-		} else {
-			println('Unknown part specified')
+		match os.args[2] {
+			'1' { println(part01(lines)!) }
+			'2' { println(part02(lines)!) }
+			else { println('Unknown part specified') }
 		}
 	} else {
-		sw := new_stopwatch()
+		mut sw := new_stopwatch()
 		println(part01(lines)!)
 		println('Part 01 took: ${sw.elapsed().microseconds()}us')
-
-		sw2 := new_stopwatch()
+		sw.restart()
 		println(part02(lines)!)
-		println('Part 02 took: ${sw2.elapsed().microseconds()}us')
+		println('Part 02 took: ${sw.elapsed().microseconds()}us')
 	}
 }
