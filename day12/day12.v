ae5a3a714a1e105d50b79d899492a9f21bb8cafd
mcbloch <git@mcbloch.dev>
2023-12-12 22:37:09 +0100

    day12 optimalizations

--- day12/day12.v
+++ day12/day12.v
@@ -12,50 +12,85 @@ fn part01(lines []string) !i64 {
 		mut springs := temp[0]
 		temp_sizes := temp[1].split(',').map(it.int())
 		mut sizes := temp_sizes.clone()
-		for i in 0..2 {
-			springs += '?'+temp[0]
-			sizes << temp_sizes
-		}
+		// for i in 0 .. 2 {
+		// 	springs += '?' + temp[0]
+		// 	sizes << temp_sizes
+		// }
 
 		println('${springs} - ${sizes}')
 
-		t := arrays.map_indexed(sizes, fn [springs, sizes] (idx int, elem int) []int {
+		mut t := arrays.map_indexed(sizes, fn [springs, sizes] (idx int, elem int) []int {
 			offset := arrays.sum(sizes[..idx].map(it + 1)) or { 0 }
 			offset_end := arrays.sum(sizes[idx + 1..].map(it + 1)) or { 0 }
 			return []int{len: (springs.len - elem + 1 - offset - offset_end), init: index + offset}
 		})
-		// println(t)
+		// Yeet pointers that we can exclude upfront.
+		// Some conditions are dependent on the combinations. They will be looked at in THE loop itself.
+		println(t)
+		for pi, mut t_opts in t {
+			mut delete_count := 0
+			for so_i, spring_offset in t_opts.clone() {
+				size := sizes[pi]
+				if springs[spring_offset..spring_offset + size].contains('.') {
+					t_opts.delete(so_i - delete_count)
+					delete_count += 1
+				}
+			}
+		}
+
+		println(t)
 		mut options := 0
 		mut pointers := []int{len: sizes.len, init: 0}
 		for {
 			// println(pointers)
 			mut valid_option := true
-			for pi, p in pointers {
-				selection := springs.substr(t[pi][p], t[pi][p] + sizes[pi])
-				// println('checking: size=${sizes[pi]}, at=${t[pi][p]}, ${selection}')
-				if selection.contains('.') || (pi > 0 && t[pi-1][pointers[pi-1]] >= t[pi][p] - 1) || (pi < pointers.len -1 && t[pi+1][pointers[pi+1]] <= (t[pi][p]+sizes[pi])) {
-					valid_option = false
-					break
-				}
+			offsets := arrays.map_indexed(pointers, fn [t] (idx int, elem int) int {
+				return t[idx][elem]
+			})
+			if arrays.window(offsets, size: 2).any(it[0] >= it[1]) {
+				valid_option = false
 			}
-			mut result := []string{len: springs.len, init: '_'}
-			for pi, p in pointers {
-				for i in 0..sizes[pi] {
-					result[t[pi][p] + i] = 'x'
-				}
-			}
-			for ci, c in springs {
-				if c == `#` && result[ci] != 'x' {
-					valid_option = false
-					break
+
+			if valid_option {
+				for pi, p in pointers {
+					selection := springs.substr(t[pi][p], t[pi][p] + sizes[pi])
+					// println('checking: size=${sizes[pi]}, at=${t[pi][p]}, ${selection}')
+					if (pi > 0 && t[pi - 1][pointers[pi - 1]] >= t[pi][p] - 1)
+						|| (pi < pointers.len - 1
+						&& t[pi + 1][pointers[pi + 1]] <= (t[pi][p] + sizes[pi])) {
+						valid_option = false
+						break
+					}
 				}
 			}
 
 			if valid_option {
-				// println(pointers)
-				// println(springs)
-				// println(result.join(''))
-				// println('')
+				mut result := []string{len: springs.len, init: '_'}
+				for pi, p in pointers {
+					for i in 0 .. sizes[pi] {
+						result[t[pi][p] + i] = 'x'
+					}
+				}
+				// Check if all fixed # characters are mapped
+				for ci, c in springs {
+					if c == `#` && result[ci] != 'x' {
+						valid_option = false
+						break
+					}
+				}
+			}
+
+			if valid_option {
+				mut result := []string{len: springs.len, init: '_'}
+				for pi, p in pointers {
+					for i in 0 .. sizes[pi] {
+						result[t[pi][p] + i] = 'x'
+					}
+				}
+				println(pointers)
+				println(springs)
+				println(result.join(''))
+				println('')
 				options += 1
 			}
 
@@ -66,13 +101,13 @@ fn part01(lines []string) !i64 {
 					pointers[i] += 1
 				}
 			}
-			if pointers[pointers.len-1] >= t[t.len - 1].len {
+			if pointers[pointers.len - 1] >= t[t.len - 1].len {
 				break
 			}
 			// println('---')
 		}
-		// println(options)
-		// println('=====')
+		println(options)
+		println('=====')
 		sum += options
 	}
 
