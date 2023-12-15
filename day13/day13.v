4dd49776b7711f889674c1f2866f25e4435e03db
mcbloch <git@mcbloch.dev>
2023-12-16 00:31:00 +0100

    day13, cleanup code

--- day13/day13.v
+++ day13/day13.v
@@ -4,7 +4,6 @@ import os
 import time { new_stopwatch }
 import math
 import arrays
-// import regex
 
 fn str_grid(lines []string) string {
 	header := []string{len: lines[0].len, init: (index + 1).str()}
@@ -22,28 +21,16 @@ fn part01(lines []string) !i64 {
 	splits << [lines.len + 1]
 
 	for pattern_range in arrays.window(splits, size: 2) {
-		println(pattern_range)
 		lines_local := lines[pattern_range[0]..pattern_range[1] - 1]
-		println(str_grid(lines_local))
-
-		println('test rows')
 		for ri := 1; ri < lines_local.len; ri += 1 {
 			a := lines_local[..ri].reverse()
 			b := lines_local[ri..]
 			compare_len := math.min(a.len, b.len)
-			// println('Comparing strings for a len of ${compare_len}')
-			// println('  ${a}')
-			// println('  ${b}')
 			if a[..compare_len] == b[..compare_len] {
-				println('match')
-				println(a[..compare_len])
-				println(ri)
-				summarize_score += (100* ri)
+				summarize_score += (100 * ri)
 			}
 		}
 
-
-		println('test columns')
 		for ci := 1; ci < lines_local[0].len; ci += 1 {
 			mut a := []string{}
 			mut b := []string{}
@@ -52,18 +39,10 @@ fn part01(lines []string) !i64 {
 				b << lines_local.map(it[ci + i]).bytestr()
 			}
 			compare_len := math.min(a.len, b.len)
-			// println('Comparing strings for a len of ${compare_len}')
-			// println('  ${a}')
-			// println('  ${b}')
 			if a[..compare_len] == b[..compare_len] {
-				println('match')
-				println(a[..compare_len])
-				println(ci)
-
 				summarize_score += ci
 			}
 		}
-		println('')
 	}
 
 	return summarize_score
@@ -79,25 +58,17 @@ fn part02(lines []string) !i64 {
 	splits << [lines.len + 1]
 
 	for pattern_range in arrays.window(splits, size: 2) {
-		println(pattern_range)
 		lines_local := lines[pattern_range[0]..pattern_range[1] - 1]
-		println(str_grid(lines_local))
 
-		println('test rows')
 		for ri := 1; ri < lines_local.len; ri += 1 {
 			a := lines_local[..ri].reverse()
 			b := lines_local[ri..]
 			compare_len := math.min(a.len, b.len)
-			// println('Comparing strings for a len of ${compare_len}')
-			// println('  ${a}')
-			// println('  ${b}')
-
 
 			mut diff_count := 0
-			for couple in arrays.group[string](a[..compare_len], b[..compare_len]){
-				// println('    ${couple}')
-				for char_i in 0..couple[0].len {
-					if couple[0][char_i] != couple[1][char_i]{
+			for couple in arrays.group[string](a[..compare_len], b[..compare_len]) {
+				for char_i in 0 .. couple[0].len {
+					if couple[0][char_i] != couple[1][char_i] {
 						diff_count += 1
 					}
 				}
@@ -105,14 +76,11 @@ fn part02(lines []string) !i64 {
 			// The smudge needs to appear in our mirroring.
 			// Technically it could also occur in the ignore row or column but that's not what they want.
 			if diff_count == 1 {
-				println('match')
-				summarize_score += ( 100* ri)
+				summarize_score += (100 * ri)
 				break
 			}
 		}
 
-
-		println('test columns')
 		for ci := 1; ci < lines_local[0].len; ci += 1 {
 			mut a := []string{}
 			mut b := []string{}
@@ -121,34 +89,20 @@ fn part02(lines []string) !i64 {
 				b << lines_local.map(it[ci + i]).bytestr()
 			}
 			compare_len := math.min(a.len, b.len)
-			// println('Comparing strings for a len of ${compare_len}')
-			// println('  ${a}')
-			// println('  ${b}')
 
 			mut diff_count := 0
-			for couple in arrays.group[string](a[..compare_len], b[..compare_len]){
-				// println('    ${couple}')
-				for char_i in 0..couple[0].len {
-					if couple[0][char_i] != couple[1][char_i]{
+			for couple in arrays.group[string](a[..compare_len], b[..compare_len]) {
+				for char_i in 0 .. couple[0].len {
+					if couple[0][char_i] != couple[1][char_i] {
 						diff_count += 1
 					}
 				}
 			}
 			if diff_count == 1 {
-				println('match')
 				summarize_score += ci
 				break
 			}
-
-			// if a[..compare_len] == b[..compare_len] {
-			// 	println('match')
-			// 	println(a[..compare_len])
-			// 	println(ci)
-
-			// 	summarize_score += ci
-			// }
 		}
-		println('')
 	}
 
 	return summarize_score
